d295362e78c672ef5b907264257b029e636d4e18
facetkte <facetkte@gmail.com>
2023-06-28 08:31:00 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/index.ts
@@ -0,0 +1,13 @@
+import Hero from "./Hero";
+import Custombutton from "./Custombutton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import CardDetails from "./CardDetails";
+import ShowMore from "./ShowMore";
+
+
+
+export { Hero, Custombutton, Navbar, Footer, CustomFilter, SearchBar, CardDetails, ShowMore }
+
